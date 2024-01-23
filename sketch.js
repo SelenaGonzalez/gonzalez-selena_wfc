bbde6eaa371e42b27effb3bde4c777d07739a911
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2024-01-23 10:33:58 -0500

    Ingreso y estructura de datos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,335 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+const celdas = [];
+const RETICULA = 10; //número de filas y columnas de la reeticula
+let ancho; //anchura de cada celda
+let alto; //altura de cada celda
 
+const azulejos = [];
+
+let opcionesI = []; //opciones de los azulejos
+
+const reglas = [
+  //reglas de los bordes de cada azulejo
+  {
+    // tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 1
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 2
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 3
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 5
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 6
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 7
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 8
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 9
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 10
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 11
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 12
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 13
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 14
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 15
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 16
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 17
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 18
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 19
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 20
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 21
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 22
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 23
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 24
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 25
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 26
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 27
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 28
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    // tile 29
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 30
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 31
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 32
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    // tile 33
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    // tile 34
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 35
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    // tile 36
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+];
+
+const NA = reglas.length; // numero de azulejos
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+  }
+}
+function setup() {
+  createCanvas(1080, 1080);
+
+  //opciones de azulejos
+  for (let i = 0; i < azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+
+  //información de celdas
+  for (let i = 0; i < RETICULA * RETICULA; i++) {
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    };
+  }
+  print(celdas);
+}
 function draw() {
   circle(mouseX, mouseY, 20);
 }
